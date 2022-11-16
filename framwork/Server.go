161c334d307c0f74626bde5ef54d6f4ddfed7701
Doby Asa <631118660@qq.com>
2022-11-16 17:39:49 +0800

    a

--- framwork/Server.go
+++ framwork/Server.go
@@ -4,21 +4,23 @@ import "net/http"
 
 type Server interface {
 	Route(pattern string, handlerFunc func(c *Context))
-	RouteWithMethod(method string, pattern string, handlerFunc func(c *Context))
+	RouteWithMethod(method string, pattern string,
+		handlerFunc func(c *Context))
 	Start(addr string) error
 }
 
 type httpServer struct {
 	Name    string
-	Handler *HandlerBasedOnMap
+	Handler Handler
 }
 
-func (s *httpServer) RouteWithMethod(method string, pattern string, handlerFunc func(c *Context)) {
-	key := s.Handler.key(method, pattern)
-	s.Handler.Roads[key] = handlerFunc
+func (s *httpServer) RouteWithMethod(method string,
+	pattern string, handlerFunc func(c *Context)) {
+	s.Handler.Route(method, pattern, handlerFunc)
 }
 
-func (s *httpServer) Route(pattern string, handlerFunc func(c *Context)) {
+func (s *httpServer) Route(pattern string,
+	handlerFunc func(c *Context)) {
 	s.RouteWithMethod(http.MethodGet, pattern, handlerFunc)
 	s.RouteWithMethod(http.MethodPost, pattern, handlerFunc)
 }
